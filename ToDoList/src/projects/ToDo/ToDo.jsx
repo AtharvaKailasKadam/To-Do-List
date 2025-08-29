cb9df1913485e8065265c73a9158190c3621e8d6
Your Name <atharvakailaskadam1809@gmail.com>
2025-08-29 21:43:32 +0530

    Done

---
+++ ToDoList/src/projects/ToDo/ToDo.jsx
@@ -0,0 +1,22 @@
+import "./ToDo.css";
+
+export const ToDo = () =>
+{
+    return <section className="todo-container">
+        <header>
+            <h1 className="todo-header">To Do List</h1>
+        </header>
+        <section className = "form">
+            <form>
+                <div>
+                    
+                </div>
+                <div>
+                    <button type="submit" className = "todo-button">
+                        Add Task
+                    </button>
+                </div>
+            </form>
+        </section>
+    </section>
+}
